74efd26d253f716602be364e75b20e8a2296a30a
George14619 <george.robinson0930@gmail.com>
2025-03-22 13:44:21 -0400

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,19 @@
+function sayHello()
+{
+    console.log("Hello there!")
+}
+
+
+
+
+
+function init()
+{
+    console.log("Hello im the init");
+    sayHello();
+}
+
+window.onload = init;//we wait until the html and css gets resolved and then execute the
+//function
+
+
