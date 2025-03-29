12f91e2b760026b616a06493543e7105c595a6f9
George14619 <george.robinson0930@gmail.com>
2025-03-29 14:56:45 -0400

    106-4_final assignment

--- scripts/app.js
+++ scripts/app.js
@@ -12,9 +12,49 @@ function saveTask(e) {
 
     let taskToSave = new Task(title, description, color, date, status, budget);
     console.log(taskToSave);
+    // save to the sever
+    $.ajax({
+        type:"POST",
+        url:"http://fsdiapi.azurewebsites.net/api/tasks/",
+        data: JSON.stringify(taskToSave),
+        contentType: "application/json",
+        success: function(response){
+            console.log(response);
+            displayTask(taskToSave);
+        },
+        error: function(error){
+            console.log("This page will autodestroy in 5 seconds", error);
+        }
+    })
 
-    displayTask(taskToSave);
+
+
+    // display logic
+    //displayTask(taskToSave);
 }
+    function loadTask() {
+        $.ajax({
+            type: "get", 
+            url: "http://fsdiapi.azurewebsites.net/api/tasks", 
+            contentType: "application/json",
+            success: function(response) {
+                console.log(response); 
+                let data = JSON.parse(response);
+                console.log(data); 
+                // just bring those messsages that was created by you
+                for(let i=0; i<data.length;i++){
+                    let list = data[i];//get every object
+                    if(list.name == "George"){
+                        displayTask(list);
+                    }
+                }
+            },
+            error: function(error) {
+                console.log("Error fetching tasks:", error);
+            }
+        });
+    }
+    
 
 function displayTask(task){
     let syntax = `
@@ -37,14 +77,27 @@ function displayTask(task){
     $("#list").append(syntax);
 }
 
+function testRequest() {
+    $.ajax({
+        type: "get",
+        url: "http://fsdiapi.azurewebsites.net",
+        success: function(x){
+            console.log(x);
+        },
+        error: function(error){
+            console.log(error);
+        }
+    })
+}
+
 function init() {
     console.log("the init is running");
 
     // Hook events
     $("#btnSave").click(saveTask);
+
+
 }
 
 window.onload = init;
-
-
 
