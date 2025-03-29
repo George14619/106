1b0084c329779d86786ec9436426d9ead9cef994
George14619 <george.robinson0930@gmail.com>
2025-03-29 09:50:04 -0400

    106-3

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,50 @@
-function saveTask()
-{
+function saveTask(e) {
+    e.preventDefault(); //this stops the page from reloading
+
     console.log("saveTask is running");
+
+    const title = $("#txtTitle").val();
+    const description = $("#txtDescription").val();
+    const color = $("#selColor").val();
+    const date = $("#txtDate").val();
+    const status = $("#txtStatus").val();
+    const budget = $("#numBudget").val();
+
+    let taskToSave = new Task(title, description, color, date, status, budget);
+    console.log(taskToSave);
+
+    displayTask(taskToSave);
 }
 
-function init()
-{
-    console.log("the init its running");
-    //load data
+function displayTask(task){
+    let syntax = `
+    <div class="task-container" style="border-color:${task.color}">
+        <div class="task">
+            <div classs="task-information">
+                <h5>${task.title}</h5>
+                <P>${task.description}</p>
+            </div>
+            
+            <div class="task-status">${task.status}</div>
 
-    //hook events
+            <div class="task-date-budget">
+                <span>${task.date}</span>
+                <span>${task.budget}</span>
+            </div>
+        </div>
+    </div>
+    `
+    $("#list").append(syntax);
+}
+
+function init() {
+    console.log("the init is running");
+
+    // Hook events
     $("#btnSave").click(saveTask);
 }
 
-
-
-
 window.onload = init;
+
+
 
